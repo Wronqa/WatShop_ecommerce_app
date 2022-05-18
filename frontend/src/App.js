52d807a2df91d91ace7d1f65c86f7a160a982a75
Wronqa <kuba.wrona@onet.pl>
2022-05-18 17:13:23 +0200

    Created login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { Register } from './views/register/Register'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Login } from './views/login/Login'
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/views/login/Login.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { useState } from 'react'
+import { Wave } from '../../components/Background/Wave'
+import { FormInput } from '../../components/FormInput/FormInput'
+import { getRegisterSchemas } from '../../utils/inputSchemas'
+import './login.css'
+
+export const Login = () => {
+  const [error, setError] = useState(null)
+
+  const [values, setValues] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setError(null)
+
+    for (let element in values) {
+      if (values[element].trim().length === 0) {
+        setError('Fields cannot be empty')
+      }
+    }
+
+    if (!error) {
+      const form = new FormData()
+
+      for (let element in values) {
+        form.append(element, values[element])
+      }
+    }
+  }
+
+  return (
+    <div className='register'>
+      <div className='register__navbar'>
+        <div className='register__navbarWrapper'>
+          <div className='register__navbarLeft'>
+            <span className='register__navbarItem'>Nazwa sklepu</span>
+          </div>
+          <div className='register__navbarRight'>
+            <span className='register__navbarItem'>Home</span>
+            <span className='register__navbarItem'>Sign in</span>
+          </div>
+        </div>
+      </div>
+      <form action='' className='register__form' onSubmit={handleSubmit}>
+        <h1 className='register__title'>Register now!</h1>
+        {getRegisterSchemas(values).map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <button className='register__button'>Register</button>
+      </form>
+      <Wave />
+    </div>
+  )
+}
